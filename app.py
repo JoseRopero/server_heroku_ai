ed532d873815ee69eacf7e42dddc119c95380311
JoseRopero <josemanuelropero@hotmail.com>
2024-10-25 17:34:20 +0200

    Ajustes

--- app.py
+++ app.py
@@ -2,25 +2,31 @@ import os
 from flask import Flask, request, jsonify
 import openai
 from flask_cors import CORS
-from dotenv import load_dotenv
 
 app = Flask(__name__)
 CORS(app)  # Permitir solicitudes desde cualquier origen
 
-load_dotenv()
-
-# Tenemos que configurar la API Key de forma segura
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
 @app.route('/chatbot-response', methods=['POST'])
 def chatbot_response():
-    data = request.get_json()
-    user_message = data.get("message")
+    user_message = request.json.get("message")
+    auth_header = request.headers.get("Authorization")
 
     if not user_message:
         return jsonify({"response": "Por favor, proporciona un mensaje válido."}), 400
 
+    if not auth_header:
+        return jsonify({'response': 'API Key de OpenAI no proporcionada'})
+
     try:
+        # Extraer la API Key del header Authorization
+        if not auth_header.startswith("Bearer "):
+            return jsonify({'response': 'Formato de autenticación inválido'}), 401
+
+        api_key = auth_header.split(' ')[1]
+
+        # Configurar la API Key para OpenAI
+        openai.api_key = api_key
+
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=user_message,
@@ -28,7 +34,7 @@ def chatbot_response():
             temperature=0.7
         )
         bot_response = response.choices[0].text.strip()
-        return jsonify({"response": bot_response}), 500
+        return jsonify({"response": bot_response})
     except Exception as e:
         return jsonify({"response": "Error al procesar tu solicitud"}), 500
 
